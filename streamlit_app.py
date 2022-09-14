73f0b5e73e6c12a266a0c6217a4561a5b29bc69c
yashwanth8978 <113495436+yashwanth8978@users.noreply.github.com>
2022-09-14 10:39:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text('Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
