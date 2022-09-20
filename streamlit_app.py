e6f469ead8b703b69cdb26a6bb5b7bfde0085e64
yashwanth8978 <113495436+yashwanth8978@users.noreply.github.com>
2022-09-20 11:37:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,3 +44,4 @@ my_cur.execute("Select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+add_my_fruit="jackfruit"
